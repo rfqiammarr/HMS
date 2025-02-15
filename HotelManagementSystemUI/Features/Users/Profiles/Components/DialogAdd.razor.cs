54625ff0b3127e954a86fd3b16005122c67c9531
Mas Ammar <rifqiammarramadhan103@gmail.com>
2025-02-15 17:44:00 +0700

    Add project files.

---
+++ HotelManagementSystemUI/Features/Users/Profiles/Components/DialogAdd.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+
+namespace HotelManagementSystemUI.Features.Users.Profiles.Components;
+
+public partial class DialogAdd
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; }
+
+    private void Submit() => MudDialog.Close(DialogResult.Ok(true));
+
+    private void Cancel() => MudDialog.Cancel();
+}
---
+++ HotelManagementSystemUI/Features/Users/Profiles/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using HotelManagementSystemUI.Features.Users.Profiles.Components;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace HotelManagementSystemUI.Features.Users.Profiles;
+public partial class Index
+{
+    [Inject]
+    private IDialogService DialogService { get; set; } = default!;
+
+    private Task OpenDialogAsync()
+    {
+        var options = new DialogOptions { CloseOnEscapeKey = true };
+        return DialogService.ShowAsync<DialogAdd>("Simple Dialog", options);
+    }
+}
